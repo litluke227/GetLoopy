e73d0f577f84e5527a7c7ab97a54a9addd08a769
litluke227 <lucasm.lm95@gmail.com>
2025-03-02 18:38:05 -0500

    Initial commit

---
+++ src/DieRoller.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class DieRollar {
+    public static void main(String[] args) {
+       Random rand = new Random();
+       int roll1 = rand.nextInt(6);
+       int roll2 = rand.nextInt(6);
+       int roll3 = rand.nextInt(6);
+
+    }
+}
+    
